3832ee60363e6e89de47511b7ec581820755278f
Gabriel Jordan <gjordan722@hotmail.com>
2023-09-24 00:28:21 -0500

    Filler screen

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div>
-    <NavBar/>
-    <div class="container-fluid" id="homePage">
-      <div class="d-flex flex-column align-items-center">
-        <div class="w-75">
-        <img class="img-fluid" src="./assets/Main_Page_First_Pic.jpg" alt="">
-        </div>
+    <!-- <NavBar/>
+    <HomePage/> -->
+    <div class="d-flex flex-column align-items-center">
+      <div class="pb-5"></div>
+      <div class="container-fluid text-center pt-5">
+        <img class="img-fluid w-75" src="./assets/Images/Aporia_BLACK-cropped.svg" alt="">
       </div>
-      <div class="">
-        <p>
-        Aporia is a New Orleans based fashion brand that sits on the crux of luxury and streetwear. The inspiration for our vision comes from a combination of stories, cultures, and ideologies shared between members who originate from all different walks of life. Young and ambitious, Aporia strives to define and redefine how creative expression and unique perspective can fuel next level design and an ever changing aesthetic. We discover and create within a state of apophenia, or a tendency to perceive meaningful connections between unrelated things. Seeking to reveal the full narrative through an all encompassing perspective, the lessons and stories that inspire our creativity lives in every thread and stitch of every piece of our incomparable apparel. 
-        </p>
-      </div>
+      <h1 id="demo" class="d-flex flex-row justify-content-center"></h1>
     </div>
   </div>
 </template>
 
 <script>
-import NavBar from './Components/AporiaNav.vue';
+// import NavBar from './Components/AporiaNav.vue';
+// import HomePage from './Components/HomePage.vue';  
 
 export default {
   components: {
-    NavBar
+    // NavBar,
+    // HomePage
   },
   data() {
     return {
       
     };
+  },
+  mounted() {
+    // Set the date we're counting down to
+var countDownDate = new Date("Sep 25, 2023 19:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
   }
 };
 </script>
+
+<style scoped>
+#demo {
+  color: white;
+  font-size: 10rem;
+  -webkit-text-stroke-width: .15rem;
+  -webkit-text-stroke-color: black;
+}
+
+#tbip {
+  font-family: 'NEXT ART Thin';
+}
+</style>
--- src/Components/AporiaNav.vue
+++ src/Components/AporiaNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex flex-column align-items-end sticky-top">
+  <div class="d-flex flex-column align-items-end sticky-top bg-light pb-3">
     <nav class="navbar navbar-expand-lg" :style="{position: absolute}">
       <button class="navbar-toggler navbartoggler-right" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -8,22 +8,22 @@
         <ul class="navbar-nav">
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <img class="img-fluid " src="../assets/A_BLACK-cropped.svg" alt="">
+              <img class="img-fluid " src="../assets/Images/A_BLACK-cropped.svg" alt="">
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <img class="img-fluid " src="../assets/SHOPPING-cropped.svg" alt="">
+              <img class="img-fluid " src="../assets/Images/SHOPPING-cropped.svg" alt="">
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <img class="img-fluid " src="../assets/CART-cropped.svg" alt="">
+              <img class="img-fluid " src="../assets/Images/CART-cropped.svg" alt="">
             </a>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <img class="img-fluid " src="../assets/INFLUENCER-cropped.svg" alt="">
+              <img class="img-fluid " src="../assets/Images/INFLUENCER-cropped.svg" alt="">
             </a>
           </li>
           <li class="nav-item">
@@ -32,22 +32,22 @@
             </a>
             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="#">
-                <img class="img-fluid " src="../assets/A_BLACK-cropped.svg" alt="">
+                <img class="img-fluid " src="../assets/Images/A_BLACK-cropped.svg" alt="">
                 About
               </a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">
-                <img class="img-fluid " src="../assets/SHOPPING-cropped.svg" alt="">
+                <img class="img-fluid " src="../assets/Images/SHOPPING-cropped.svg" alt="">
                 Store
               </a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">
-                <img class="img-fluid " src="../assets/CART-cropped.svg" alt="">  
+                <img class="img-fluid " src="../assets/Images/CART-cropped.svg" alt="">  
                 Cart
               </a>
               <div class="dropdown-divider"></div>
               <a class="dropdown-item" href="#">
-                <img class="img-fluid " src="../assets/INFLUENCER-cropped.svg" alt="">
+                <img class="img-fluid " src="../assets/Images/INFLUENCER-cropped.svg" alt="">
                 Influencers
               </a>
             </div>
@@ -57,10 +57,10 @@
     </nav>
     <div class="container-fluid text-center" :style="{height: '15vh'}">
       <a href="#">
-        <img class="img-fluid brand" src="../assets/Site_Deesign_Assets-09-cropped.svg" alt="">
+        <img class="img-fluid brand" src="../assets/Images/Site_Deesign_Assets-09-cropped.svg" alt="">
       </a>
     </div>
-    <hr class="container-fluid" :style="{width: '90vw', borderTop: '.55rem solid #ffffff'}"/>
+    <!-- <hr class="container-fluid" :style="{width: '90vw', borderTop: '.55rem solid #000000'}"/> -->
   </div>
 </template>
 
