19c524299db4afce6bdcbcec5d87b3d742aa1ab9
Gabriel Jordan <gjordan722@hotmail.com>
2023-09-26 02:17:11 -0500

    timer extend

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
   },
   mounted() {
     // Set the date we're counting down to
-    var countDownDate = new Date("Sep 27, 2023 20:00:00").getTime();
+    var countDownDate = new Date("Oct 13, 2023 20:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
